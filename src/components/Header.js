3f0fd8610044adecce9c0be0203ef9d083eeb559
nison79 <georgenison@gmail.com>
2021-04-23 13:46:53 +0300

    Sign In Sign Out Function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useEffect } from 'react'
 import styled from 'styled-components'
 import logo from '../assets/images/logo.svg'
 import home from '../assets/images/home-icon.svg'
@@ -10,23 +10,44 @@ import series from '../assets/images/series-icon.svg'
 import { auth, provider } from '../firebase'
 import { useDispatch , useSelector} from 'react-redux'
 import { useHistory } from 'react-router-dom'
-import { selectUserName , setUserLoginDetails, selectUserPhoto} from '../features/user/userSlice'
+import { selectUserName , setUserLoginDetails, selectUserPhoto, setSignOutState} from '../features/user/userSlice'
 
 const Header = () => {
-   const dispatch = useDispatch()
-   const history = useHistory()
-  const username = useSelector(selectUserName)
-  const userphoto = useSelector(selectUserPhoto)
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const username = useSelector(selectUserName)
+    const userphoto = useSelector(selectUserPhoto)
+
+    //if the user exists bring him to the home page
+    useEffect(() => {
+      auth.onAuthStateChanged(async (user) => {
+        if(user) {
+          setUser(user);
+          history.push('./home');
+        }
+      });
+     }, [username]);
 
 
 
   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result) => {
+    if (!username ) {
+    auth
+    .signInWithPopup(provider)
+    .then((result) => {
       setUser(result.user);
+
     }).catch((error) => {
       alert(error.message)
     })
+    
+  } else if(username) {
+    auth.signOut().then(() => {
+      dispatch(setSignOutState())
+      history.push('/')
+    }).catch((error) => alert(error.me))
   }
+}
 
 
   const setUser = (user) => {
@@ -76,7 +97,12 @@ const Header = () => {
           <span>SERIES</span>
         </a>
       </NavMenu>
-      <UserImg src={userphoto} alt={username} />
+      <SignOut>
+        <UserImg src={userphoto} alt={username} />
+        <DropDown>
+          <span onClick={handleAuth}>SignOut</span>
+        </DropDown>
+      </SignOut>
       </>
       }
     </Nav>
@@ -194,7 +220,42 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height:100%;
-  border-radius:50%;
+`
+
+const DropDown = styled.div`
+  position:absolute;
+  top:48px;
+  right:20px;
+  background: rgb(19,19,19);
+  border: 1px solid rgba(151,151,151,0.34);
+  border-radius:4px;
+  box-shadow:rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding:10px;
+  font-size:14px;
+  letter-spacing:3px;
+  width:100%;
+  opacity:0;
+`
+
+const SignOut = styled.div`
+    position:relative;
+    height:48px;
+    width:48px;
+    display:flex;
+    cursor:pointer;
+    align-items:center;
+    justify-content:center;
+    ${UserImg} {
+      border-radius: 50%;
+      width:100%;
+      height:100%;
+    }
+    &:hover {
+      ${DropDown} {
+        opacity:1;
+        transition-duration:1s;
+      }
+    }
 `
 
 export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default Home
