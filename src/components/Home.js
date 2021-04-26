93349f54d022c554b8348c50a5fefe52f2ed9bfb
nison79 <georgenison@gmail.com>
2021-04-26 11:50:47 +0300

    firebase db in JSON-REDUX

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
 import Recommends from "./Recommends";
+import Trending from "./Trending";
 import Viewers from "./Viewers";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = () => {
-  
+  const dispatch = useDispatch();
+  const username = useSelector(selectUserName);
+  let recommends = [];
+  let newDisney = [];
+  let originals = [];
+  let trending = [];
   return (
     <Container>
       <ImgSlider />
       <Viewers />
       <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
